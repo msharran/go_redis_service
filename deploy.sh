a55f506a52da9bb663e04840609d34ba75b126fd
Sharran <sharran.murali@gmail.com>
2021-06-08 13:06:53 +0530

    #refractor

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,12 @@ docker run --net=host --rm -v "$PWD":/usr/src/app -w /usr/src/app golang:1.16 go
 echo "Pushing container........"
 docker push sharran/go_redis_app  || exit
 
-# #deploy
-# echo "Provisioning server........"
-# cd terraform || exit
-# terraform init  || exit
-# terraform validate  || exit
-# terraform plan || exit
-# terraform apply -auto-approve
-# echo "Successfully deployed........"
-# echo "It will take around 2 to 5 mins for the server to be up and running. Please wait..."
+#deploy
+echo "Provisioning server........"
+cd terraform || exit
+terraform init  || exit
+terraform validate  || exit
+terraform plan || exit
+terraform apply -auto-approve
+echo "Successfully deployed........"
+echo "It will take around 2 to 5 mins for the server to be up and running. Please wait..."
